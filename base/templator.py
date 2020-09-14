c2149cf2dbce8d328b2fcb1f4780d8b195e3cad1
Sergey Semyonov <evlx@mail.ru>
2020-09-14 20:51:22 +0300

    add base template, correct templates to work with base.html

--- base/templator.py
+++ base/templator.py
@@ -1,13 +1,20 @@
 import os
 
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 from base.settings import TEMPLATES
 
 
+# def render(template_name, folder=TEMPLATES, **kwargs):
+#     file_path = os.path.join(folder, template_name)
+#
+#     with open(file_path, encoding='utf-8') as file:
+#         template = Template(file.read())
+#
+#     return template.render(**kwargs)
+#
+
 def render(template_name, folder=TEMPLATES, **kwargs):
-    file_path = os.path.join(folder, template_name)
-
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
-
-    return template.render(**kwargs)
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    tmpl = env.get_template(template_name)
+    return tmpl.render(**kwargs)
